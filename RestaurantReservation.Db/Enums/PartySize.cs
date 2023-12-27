f896cebd0edbeae2c4a2410f757633bfb5eb8237
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2023-12-27 22:51:59 +0200

    create the data models

---
+++ RestaurantReservation.Db/Enums/PartySize.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RestaurantReservation.Db.Enums
+{
+    internal enum PartySize
+    {
+        small = 1,
+        mediam = 2,
+        large = 3,
+    }
+}
--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,13 +1,48 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using RestaurantReservation.Db.Models;
 
 namespace RestaurantReservation.Db
 {
     internal class RestaurantReservationDbContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Table> Tables { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=RestaurantReservationCore;Integrated Security=True");
+            optionsBuilder.UseSqlServer("Server=TASBEH-TAKRORE;Database=RestaurantReservationCore;Trusted_Connection=True;TrustServerCertificate=True;",
+                x => x.UseDateOnlyTimeOnly());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+              .OwnsOne(r => r.OpeningHours, oh =>
+              {
+                  oh.Property(wh => wh.StartTime).HasColumnName("StartTime");
+                  oh.Property(wh => wh.EndTime).HasColumnName("EndTime");
+              });
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.Table)
+                .WithMany(t => t.Reservations)
+                .HasForeignKey(r => r.TableId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Reservation>()
+                .HasMany(r => r.Orders)
+                .WithOne(t => t.Reservation)
+                .HasForeignKey(r => r.OrderId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<OrderItem>()
+                 .HasOne(oi => oi.Order)
+                 .WithMany(o => o.OrderItems)
+                 .HasForeignKey(oi => oi.OrderId)
+                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
