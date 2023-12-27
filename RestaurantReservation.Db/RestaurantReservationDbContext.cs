f8d8c666b674d7cde8b83e56a67f558ee3f42d29
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2023-12-27 09:16:16 +0200

    Create RestaurantReservationDbContext inside RestaurantReservation.Db

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace RestaurantReservation.Db
+{
+    internal class RestaurantReservationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=RestaurantReservationCore;Integrated Security=True");
+        }
+    }
+}
