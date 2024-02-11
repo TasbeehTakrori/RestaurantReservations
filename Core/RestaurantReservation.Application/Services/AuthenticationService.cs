2c59aab3f71372dd7a03b3e0abbeea06fb45c6bb
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2024-02-11 20:50:09 +0200

    Add AuthenticationService

---
+++ Core/RestaurantReservation.Application/Services/AuthenticationService.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantReservation.Application.Entities;
+using RestaurantReservation.Application.Services.IServices;
+
+namespace RestaurantReservation.Application.Services
+{
+    internal class AuthenticationService
+    {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public string GenerateJwtToken(User user)
+        {
+            return _jwtTokenGenerator.GenerateToken(user);
+        }
+    }
+}
